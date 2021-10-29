b321f495f898297fcc89e1128be43ed928b10713
Ford Laturnas <fordlaturnas1209@gmail.com>
2021-10-29 20:25:55 +0800

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <h1>{{title}}</h1> 
+ <Modal :fordyBind="fordy" :textFordyBind="textfordy" themeProp="sale"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+     return {
+        title:'My First Vue App :)',
+        fordy: 'FOOOORRDD',
+        textfordy: 'yeah lets try how this props works hahahah'
+    }
   }
+  
 }
 </script>
 
@@ -22,5 +30,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="backdrop">
+        <div class="modal" :class="{ sale: themeProp === 'sale'}">
+            <h1>{{fordyBind}}</h1>
+            <p>{{textFordyBind}}</p> 
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+   props: ['fordyBind','textFordyBind', 'themeProp']
+}
+</script>
+
+
+<style>
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop {
+        top: 0;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+    .modal h1 {
+       color: #03cfb4;
+       border: none;
+       padding: 0;
+    }
+    .modal p {
+        font-style: normal;
+    }
+
+    .modal.sale {
+        background:crimson;
+        color: white;
+    }
+    .modal.modal.sale.h1 {
+        color: white;
+    }
+</style>
